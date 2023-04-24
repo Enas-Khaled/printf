c1988b598d25c0d6eb6508b8f64693a4602b160a
sayed <saidmo19734@gmail.com>
2023-04-24 00:40:16 -0700

    intial commit

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define NOT_USED(x) (void)(x)
+#define THE_BUFFER 1024
+
+#define STR_NULL "(null)"
+
+
+/* _task1 */
+int _printf(const char *format, ...);
+
+
+#endif
