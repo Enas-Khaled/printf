21324ca7a8cfbcd5a7be7b7d8c963a25f2b8f080
sayedabdelal <saidmo19734@gmail.com>
2023-04-24 16:52:53 -0700

    intial commit

---
+++ printf_string_fields.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_precision - gets the precision from the format string
+ * @p: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ *
+ * Return: new pointer
+ */
+char *get_precision(char *p, params_t *params, va_list ap)
+{
+	int di = 0;
+
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		di = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			di = di * 10 + (*p++ - '0');
+	}
+	params->precision = di;
+	return (p);
+}
