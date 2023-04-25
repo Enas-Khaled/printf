0cd2c6d4e246eea186a7fa3908fd46b6b4be04e3
sayedabdelal <saidmo19734@gmail.com>
2023-04-24 17:24:03 -0700

    intial commit

---
+++ printf_specifier.c
@@ -0,0 +1,137 @@
+#include "main.h"
+
+/**
+ * my_get_specifier - finds the format func
+ * @s: the format string
+ *
+ * Return: the number of bytes printed
+ */
+int (*my_get_specifier(char *s))(va_list ap, params_t *params)
+{
+	specifier_t specifiers[] = {
+		{"c", my_print_char},
+		{"d", my_print_int},
+		{"i", my_print_int},
+		{"s", print_custom_string},
+		{"%", print_percent_new},
+		{"b", printUnsignedBinary},
+		{"o", print_custom_octal},
+		{"u", print_my_unsigned},
+		{"x", print_hex_lower},
+		{"X", print_hex_upper},
+		{"p", my_print_address},
+		{"S", print_custom_S},
+		{"r", print_reverse},
+		{"R", print_rot13_string},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * toget_my_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: the number of bytes printed
+ */
+int toget_my_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * toget_my_flag - finds the flag func
+ * @s:  format string
+ * @params:  parameters struct
+ *
+ * Return: if flag was valid
+ */
+int toget_my_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * toget_my_modifier - finds the modifier func
+ * @s: format string
+ * @params: parameters struct
+ *
+ * Return: modifier  valid or not
+ */
+int toget_my_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		i = params->h_modifier = 1;
+		break;
+	case 'l':
+		i = params->l_modifier = 1;
+		break;
+	}
+	return (i);
+}
+
+/**
+ * toget_my_width - gets the width from the format string
+ * @s: format string
+ * @params: parameters struct
+ * @ap: argument pointer
+ *
+ * Return: new pointer
+ */
+char *toget_my_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
